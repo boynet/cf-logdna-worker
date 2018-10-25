07e6539c476ac6fb169aae4a29004adec1cd1b0e
Hugo Romano <hugoromano@gmail.com>
2018-10-25 13:15:54 +0100

    spelling correction and timestamp change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let requests = [];
 let lastTimeSent = Date.now();
 const requestsPerBatch = 50;
-const maxReuqestsAge = 60000; //in milliseconds
+const maxRequestsAge = 60000; //in milliseconds
 
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event))
@@ -14,7 +14,7 @@ addEventListener('fetch', event => {
 async function handleRequest(event) {
     if (!lastTimeSent) lastTimeSent = Date.now();
     requests.push(getRequestData(event.request));
-    if (requests.length >= requestsPerBatch || (Date.now() - lastTimeSent >= maxReuqestsAge)) {
+    if (requests.length >= requestsPerBatch || (Date.now() - lastTimeSent >= maxRequestsAge)) {
         try {
             event.waitUntil(postRequests({'lines': requests}))
         } catch (err) {
@@ -28,16 +28,16 @@ async function handleRequest(event) {
 function getRequestData(request) {
     let data = {
         'app': 'myApp',
+        'timestamp' : Date.now(),
         'meta': {
-            'ua' : request.headers.get('user-agent'),
+            'ua': request.headers.get('user-agent'),
             'referer' : request.headers.get('Referer') || 'empty',
             'ip' : request.headers.get('CF-Connecting-IP'),
             'countryCode' : request.headers.get("cf-ipcountry"),
             'url' : request.url,
             'method' : request.method,
             'x_forwarded_for' : request.headers.get('x_forwarded_for') || "0.0.0",
-            'asn' : (request.cf || {}).asn,
-            'timestamp' : Date.now()
+            'asn' : (request.cf || {}).asn
         }
     };
     data.line = data.meta.countryCode + " " + data.meta.ip + " " + data.meta.url;
@@ -46,16 +46,17 @@ function getRequestData(request) {
 
 async function postRequests(data) {
     data = JSON.stringify(data);
-    let username = 'My logdna Ingestion key';
-    let password = '';
-    let compiledPass = "";
+    const username = 'My logdna Ingestion key';
+    const password = '';
+    const compiledPass = '';
+    const hostname = 'example.com';
     let myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json; charset=UTF-8');
     myHeaders.append('Authorization', 'Basic ' + (compiledPass || btoa(username+':'+password)));
     requests = [];
     lastTimeSent = Date.now();
     try {
-        return fetch('https://logs.logdna.com/logs/ingest?hostname=myHostName', {
+        return fetch('https://logs.logdna.com/logs/ingest?tag=worker&hostname='+ hostname +'&now=' + lastTimeSent, {
             method: 'POST',
             headers: myHeaders,
             body: data
