3eec90c326ff5af8a6e1e62636d39fe356f2cf2d
Hugo Romano <hugoromano@gmail.com>
2018-10-29 04:04:27 +0000

    logs most recent event as well

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function handleRequest(event) {
     if (!lastTimeSent) lastTimeSent = Date.now();
     if (!workerInception) workerInception = Date.now();
     if (!workerId) workerId = makeid(6);
-    requests.push(getRequestData(event.request));
+    await requests.push(getRequestData(event.request));
     if (requests.length >= requestsPerBatch || (Date.now() - lastTimeSent >= maxRequestsAge)) {
         try {
             event.waitUntil(postRequests({'lines': requests}))
